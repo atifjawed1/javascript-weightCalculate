0e7fb79fe22f1a1d6d61e85f7d4476d0ddecef30
atifjawed1 <bmj.atifjawed@gmail.com>
2024-02-15 01:12:20 +0500

    make-weight-calculate-logic

---
+++ script.js
@@ -0,0 +1,8 @@
+// prompt the user to enter the weight of chicken in grams 
+var weightInGrams = prompt("Enter the weight of chicken (in grams):");
+// convert weight into kg 
+var WeightInKg = weightInGrams / 1000;
+
+var price = WeightInKg * 1000; 
+// price per kg is 500
+document.write("You have to pay"+ price +"rupees for"+ weightInGrams +"grams for chicken")
