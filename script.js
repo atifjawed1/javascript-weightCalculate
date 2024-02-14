03915cb2703eaa2849fdebc496847aea5329035a
atifjawed1 <bmj.atifjawed@gmail.com>
2024-02-15 01:29:11 +0500

    change value of price

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ var weightInGrams = prompt("Enter the weight of chicken (in grams):");
 // convert weight into kg 
 var WeightInKg = weightInGrams / 1000;
 
-var price = WeightInKg * 1000; 
+var price = WeightInKg * 500; 
 // price per kg is 500
 document.write("You have to pay"+ price +"rupees for"+ weightInGrams +"grams for chicken")
